4703ba64bc21696857b13367ade8694b8e156d46
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2026-03-28 16:10:56 +0000

    Adding SQLC and registering new users

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"github.com/geirgulbrandsen/gator/internal/config"
+	"github.com/geirgulbrandsen/gator/internal/database"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
-	config *config.Config
+	db  *database.Queries
+	cfg *config.Config
 }
 
 type command struct {
@@ -40,11 +48,55 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	username := cmd.args[0]
-	if err := s.config.SetUser(username); err != nil {
+	_, err := s.db.GetUser(context.Background(), username)
+	if errors.Is(err, sql.ErrNoRows) {
+		return fmt.Errorf("user %s does not exist", username)
+	}
+	if err != nil {
+		return fmt.Errorf("checking user: %w", err)
+	}
+
+	if err := s.cfg.SetUser(username); err != nil {
 		return err
 	}
 
 	fmt.Printf("User set to %s\n", username)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("username is required")
+	}
+
+	name := cmd.args[0]
+
+	_, err := s.db.GetUser(context.Background(), name)
+	if err == nil {
+		return fmt.Errorf("user %s already exists", name)
+	}
+	if !errors.Is(err, sql.ErrNoRows) {
+		return fmt.Errorf("checking for existing user: %w", err)
+	}
+
+	now := time.Now().UTC()
+	user, err := s.db.CreateUser(context.Background(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      name,
+	})
+	if err != nil {
+		return err
+	}
+
+	if err := s.cfg.SetUser(name); err != nil {
+		return err
+	}
+
+	fmt.Printf("User created: %s\n", name)
+	log.Printf("created user: %+v", user)
+
 	return nil
 }
 
@@ -60,12 +112,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	s := &state{config: &cfg}
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error opening database: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	dbQueries := database.New(db)
+
+	s := &state{
+		db:  dbQueries,
+		cfg: &cfg,
+	}
 
 	cmds := &commands{
 		handlers: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	cmd := command{
 		name: os.Args[1],
