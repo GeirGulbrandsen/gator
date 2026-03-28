87984c3bb6a9d803a2fcf347b8616051321fe771
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2026-03-28 15:19:07 +0000

    Added login handler and dealign with commands

--- main.go
+++ main.go
@@ -1,26 +1,80 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"log"
+	"os"
 
 	"github.com/geirgulbrandsen/gator/internal/config"
 )
 
+type state struct {
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.handlers[cmd.name]
+	if !ok {
+		return fmt.Errorf("unknown command: %s", cmd.name)
+	}
+
+	return handler(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("username is required")
+	}
+
+	username := cmd.args[0]
+	if err := s.config.SetUser(username); err != nil {
+		return err
+	}
+
+	fmt.Printf("User set to %s\n", username)
+	return nil
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "not enough arguments provided")
+		os.Exit(1)
+	}
+
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatalf("reading config: %v", err)
+		fmt.Fprintf(os.Stderr, "error reading config: %v\n", err)
+		os.Exit(1)
 	}
 
-	if err := cfg.SetUser("John Constantine"); err != nil {
-		log.Fatalf("setting user: %v", err)
+	s := &state{config: &cfg}
+
+	cmds := &commands{
+		handlers: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
 	}
 
-	updatedCfg, err := config.Read()
+	err = cmds.run(s, cmd)
 	if err != nil {
-		log.Fatalf("re-reading config: %v", err)
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
 	}
-
-	fmt.Printf("%+v\n", updatedCfg)
 }
